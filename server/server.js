5d1b0bfa6202cde3fb16ff6ea2269aba8e052631
sikanderv <sikanderv@gmail.com>
2018-02-08 00:35:46 -0600

    Set up for Heroku deployment

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 let app = express();
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`Server is up and running on port ${port}`);
 });
 
